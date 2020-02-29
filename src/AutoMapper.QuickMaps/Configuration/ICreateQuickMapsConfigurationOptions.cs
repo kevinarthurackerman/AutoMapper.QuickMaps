ac1afb208988b2336f4077997b6b81fab4dc3b46
kackerman <kevin.arthur.ackerman@gmail.com>
2020-02-28 21:12:41 -0600

    Initial commit
    Pulling this project out of RemotiatR repo

---
+++ src/AutoMapper.QuickMaps/Configuration/ICreateQuickMapsConfigurationOptions.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper.QuickMaps.MappingMatchers;
+using System;
+using System.Reflection;
+
+namespace AutoMapper.QuickMaps.Configuration
+{
+    public interface ICreateQuickMapsConfigurationOptions
+    {
+        ICreateQuickMapsConfigurationOptions AddAssemblies(params Assembly[] assemblies);
+
+        ICreateQuickMapsConfigurationOptions AddAssemblies(params Type[] assemblyTypeMarkers);
+
+        ICreateQuickMapsConfigurationOptions AddMappingMatchers(params MappingMatcher[] mappingMatchers);
+    }
+}
---
+++ src/AutoMapper.QuickMaps/MappingMatchers/MappingMatcher.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace AutoMapper.QuickMaps.MappingMatchers
+{
+    public delegate bool MappingMatcher(Type sourceType, Type destinationType);
+}
